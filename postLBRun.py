10087f5e9257e973ae8f67731f7e03a294d913b8
Ezequiel O. Fogliatto <ezefogliatto@gmail.com>
2018-02-01 14:22:49 -0300

    Modificaciones a postLBRun.scalarProfile para buscar directamente en \'ultimo paso de tiempo

--- postLBRun.py
+++ postLBRun.py
@@ -2,11 +2,55 @@ import numpy as np
 
 import matplotlib.pyplot as plt
 
+import glob
+
+
+
 
 # Load 1D profile from scalar field
 
-def scalarProfile( fname, step = 3, offset = 0 ):
+def scalarProfile( folderName, fieldName, step = 3, offset = 0, time = 'latest' ):
 
+    if folderName[-1] == '/':
+
+        fname = folderName + 'processor0/'
+
+    else:
+            
+        fname = folderName + '/processor0/'
+
+
+        
+    # Check for latest time
+
+    if time == 'latest':
+
+            
+        flist = glob.glob( fname + '*' )
+
+        
+        # Trim name
+
+        tlist = []
+
+        for a in flist:
+            
+            if a[len(fname):].isdigit():
+
+                tlist.append(  int( a[len(fname):] )  )
+        
+
+        fname = fname + '{}'.format( sorted(tlist)[-1] ) + '/' + fieldName 
+
+
+
+    else:
+
+        fname = fname + '/' + time + '/' + fieldName
+
+
+    
+    
 
     fileField = np.loadtxt( fname, unpack = True)[1:]
     
@@ -17,8 +61,6 @@ def scalarProfile( fname, step = 3, offset = 0 ):
     idx = range( offset, len(fileField), step )
     
     field = np.zeros( len(idx) )
-
-
     
     for i,j in enumerate(idx):
 
